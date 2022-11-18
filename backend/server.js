38e642791fff7aa5e1a21da0f491a4433efa526d
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-18 19:51:48 +0100

    Added new backend branch and setup express

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/', function(req,res){
+    res.send('Hello World')
+})
+
+app.listen(process.env.SERVER_PORT)
