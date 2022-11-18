80125142caca1c5377c9fd7d7a7119b7a13283c7
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-18 14:50:01 +0100

    Project Setup

---
+++ svendeproeve/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+        <h2 className='font-bold text-3xl'>Test</h2>
+    </div>
+  )
+}
+
+export default App
