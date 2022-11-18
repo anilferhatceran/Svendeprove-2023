001d6047b3d73b4233658ff0c1182f2a787b40d6
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-18 19:36:36 +0100

    Getting ready for code

--- svendeproeve/src/App.jsx
+++ svendeproeve/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   return (
     <div>
         <Header/>
-        <h2>Testing if the branch "Anil" works and whether the merging works.</h2>
-        <h2>Testing again.</h2>
     </div>
   )
 }
