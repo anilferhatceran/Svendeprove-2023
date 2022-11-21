989d108c50a723cf8cef9aec67cd48bfc26d4084
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-21 08:22:59 +0100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-connectDB()
-
+connectDB();
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-})
+  console.log(`Server started on port ${port}`);
+});
