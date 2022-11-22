85225d0f09e1a95731b4949d1f3183fec12d8ed1
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-22 12:03:41 +0100

    Added basic CRUD functionality

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+// @desc    Get users
+// @route   GET /api/users
+// @access  Private
+const getUsers = (req, res) => {
+  res.status(200).json({ message: "Get Users" });
+};
+
+// @desc    Set user
+// @route   POST /api/users
+// @access  Private
+const postUser = (req, res) => {
+  res.status(200).json({ message: "Created User" });
+};
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private
+const updateUser = (req, res) => {
+  res.status(200).json({ message: `Updated User ${req.params.id}` });
+};
+// @desc    Delete user
+// @route   DELETE /api/users
+// @access  Private
+const deleteUser = (req, res) => {
+  res.status(200).json({ message: `Deleted User ${req.params.id}` });
+};
+module.exports = {
+  getUsers,
+  postUser,
+  updateUser,
+  deleteUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const dotenv = require("dotenv").config();
 const express = require("express");
-const app = express();
-const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const app = express();
+const connectDB = require("./config/db");
+
 connectDB();
+
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
