c59d1a17af1924d9df0a238786cd51e92a4f786d
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-22 14:31:23 +0100

    Full CRUD setup and connection with DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,32 +1,63 @@
+const asyncHandler = require("express-async-handler");
+const { runInNewContext } = require("vm");
+
+const User = require("../models/userModel");
+
 // @desc    Get users
 // @route   GET /api/users
 // @access  Private
-const getUsers = async (req, res) => {
-  res.status(200).json({ message: "Get Users" });
-};
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json(users);
+});
 
 // @desc    Set user
 // @route   POST /api/users
 // @access  Private
-const postUser = async (req, res) => {
-  if (!req.body.text) {
+const postUser = asyncHandler(async (req, res) => {
+  if (!req.body.username || !req.body.email) {
     res.status(400);
     throw new Error("Please add a user");
   }
-  res.status(200).json({ message: "Created User" });
-};
+
+  const user = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+  });
+
+  res.status(200).json(user);
+});
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private
-const updateUser = async (req, res) => {
-  res.status(200).json({ message: `Updated User ${req.params.id}` });
-};
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found");
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updateUser);
+});
 // @desc    Delete user
 // @route   DELETE /api/users
 // @access  Private
-const deleteUser = async (req, res) => {
-  res.status(200).json({ message: `Deleted User ${req.params.id}` });
-};
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found");
+  }
+
+  await user.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 module.exports = {
   getUsers,
   postUser,
