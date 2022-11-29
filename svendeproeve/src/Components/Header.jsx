f199b22c5ffbb42f7e18b0269fb7e463a9f0995b
Anil Ferhat Ceran <69785410+anilferhatceran@users.noreply.github.com>
2022-11-29 18:57:44 +0100

    Added user menu UI

--- svendeproeve/src/Components/Header.jsx
+++ svendeproeve/src/Components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaHouseUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+  const [userMenu, setUserMenu] = useState(false);
+
   return (
     <nav className="font-DMsans m-5">
       <div className="flex flex-wrap items-center  justify-between">
@@ -30,16 +33,46 @@ export default function Header() {
         {/* <select className="h-10 w-10 rounded-full bg-blue-500 appearance-none">
           <option>Sign In</option>
         </select> */}
-        <button className="block h-9 w-9 rounded-full overflow-hidden border-2 border-gray-100 focus:outline-none focus:border-gray-500">
-          <img
-            className="h-full w-full object-cover"
-            src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/profile-icon.png"
-          />
-        </button>
-        <div className="bg-gray-100 rounded-lg p-4 py-2">
-          <Link className="block py-1 hover:bg-gray-200">Dashboard</Link>
-          <Link className="block py-1">Sign In</Link>
-          <Link className="block py-1">Sign Out</Link>
+        <div>
+          {userMenu ? (
+            <>
+              <button
+                onClick={() => setUserMenu(!userMenu)}
+                className="block h-9 w-9 rounded-full overflow-hidden border-2 border-gray-100 focus:outline-none hover:border-gray-500"
+              >
+                <img
+                  className="h-full w-full object-cover"
+                  src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/profile-icon.png"
+                />
+              </button>
+              <div className="bg-gray-100 rounded-lg mt-1 ">
+                <Link
+                  className="block py-1.5 p-3 hover:bg-gray-200 w-full rounded-md mb-1"
+                  to="/dashboard"
+                >
+                  Dashboard
+                </Link>
+                <Link className="block py-1.5 p-3 hover:bg-gray-200 rounded-md mb-1">
+                  Sign In
+                </Link>
+                {/* <Link className="block py-1 p-3 hover:bg-gray-200 rounded-md">
+                  Sign Out
+                </Link> */}
+              </div>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setUserMenu(!userMenu)}
+                className="block h-9 w-9 rounded-full overflow-hidden border-2 border-gray-100 focus:outline-none hover:border-gray-500"
+              >
+                <img
+                  className="h-full w-full object-cover"
+                  src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/profile-icon.png"
+                />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </nav>
