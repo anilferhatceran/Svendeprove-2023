77570cdc4d5439be415ee5c65353779629cdbcfc
Oliver <oliverpatrickhansen@gmail.com>
2022-12-01 14:53:20 +0100

    Started implementing React-Spring

--- svendeproeve/src/Components/CasePage.jsx
+++ svendeproeve/src/Components/CasePage.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import dummyData from "../dummyData";
 import ImageCarousel from "./ImageCarousel";
 
+
 export default function CasePage(){
+
     return(
         <div className="bg-white h-screen mt-10">
-            <div className="w-full bg-slate-300">
+
+            <div className=" bg-slate-300">
                 <ImageCarousel slides={dummyData}/>
             </div>
         </div>
--- svendeproeve/src/Components/ImageCarousel.jsx
+++ svendeproeve/src/Components/ImageCarousel.jsx
@@ -1,34 +1,80 @@
 import React,{useState} from 'react'
 import dummyData from '../dummyData'
 import {FaArrowCircleRight,FaArrowCircleLeft} from 'react-icons/fa'
+import { useSpring, animated, to } from "react-spring";
 
 const ImageCarousel = ({slides}) => {
     const[current,setCurrent] = useState(0)
+    const[prev, setPrev] = useState(slides.length-1)
     const length = slides.length
 
     if(!Array.isArray(slides)  || slides.length <= 0){
         return null
     }
-
+    console.log("current: " +current);
+    console.log("prev: " +prev);
+    const [springs, api] = useSpring(() => ({
+        from: { x: 0 },
+      }))
     
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1)
+        setPrev(prev === length - 1 ? 0 : prev  + 1)
+        api.start({
+            from: {
+              x: 1500,
+            },
+            to: {
+              x: 0,
+            },
+          })
     }
 
     const prevSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1)
+        setPrev(prev === 0 ? length - 1 : prev - 1)
+        api.start({
+            from: {
+              x: -1500,
+            },
+            to: {
+              x: 0,
+            },
+          })
     }
     return(
-    <section className='relative h-[100vh] flex justify-center  '>
-        <FaArrowCircleLeft className='absolute top-1/2 left-8 text-5xl text-white z-10 cursor-pointer select-none' onClick={prevSlide}/>
-        <FaArrowCircleRight className='absolute top-1/2 right-8 text-5xl text-white z-10 cursor-pointer select-none' onClick={nextSlide}/>
-        {dummyData.map((slide,index) =>{
-        return(
-            <div className={index === current ? 'opacity-100 transition duration-100 scale-100' : 'opacity-0 transition duration-100 ease-out'} key={index}>
-                {index === current && (<img src={slide.image} className="w-[1000px] h-[600px]"/>)}
-            </div>
-            )
-        })}
+    <section className='relative   flex justify-center '>
+        <div className='w-[1000px] h-[600px] relative overflow-hidden'>
+        <FaArrowCircleLeft className=' absolute top-1/2 left-8 text-5xl text-white z-50 cursor-pointer select-none' onClick={prevSlide}/>
+        <FaArrowCircleRight className='absolute top-1/2 right-8 text-5xl text-white z-50 cursor-pointer select-none' onClick={nextSlide}/>
+            {dummyData.map((slide,index) =>{
+            return(
+                <div className='' key={index}>
+                    <animated.div
+                    style={{
+                        width: 1000,
+                        ...springs,
+                    }}
+                    >
+                        <div className={'z-20 absolute'} >
+                            {index === current && (<img src={slide.image} className="w-[1000px] h-[600px]"/>)}
+                        </div>
+                    </animated.div>
+                    
+                    <animated.div
+                    style={{
+                        width: 1000,
+                        ...springs,
+                    }}
+                    >
+                        <div className={'z-10 absolute'} >
+                            {index === prev && (<img src={slide.image} className="w-[1000px] h-[600px]"/>)}
+                        </div>
+                    </animated.div>
+                </div>
+                )
+            })}
+        </div>
     </section>
   ) 
 }
